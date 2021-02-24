c7f9792c00600ab3e2a69a5f24ba3a9c88520836
thijs <thijs.goettsch@gmail.com>
2021-02-24 22:05:30 +0100

    equals, hashcode and tostring added

--- src/main/java/nl/hu/cisq1/lingo/words/domain/Word.java
+++ src/main/java/nl/hu/cisq1/lingo/words/domain/Word.java
@@ -3,6 +3,7 @@ package nl.hu.cisq1.lingo.words.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity(name = "words")
 public class Word {
@@ -23,5 +24,27 @@ public class Word {
 
     public Integer getLength() {
         return length;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word = (Word) o;
+        return Objects.equals(value, word.value) &&
+                Objects.equals(length, word.length);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, length);
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "value='" + value + '\'' +
+                ", length=" + length +
+                '}';
     }
 }
